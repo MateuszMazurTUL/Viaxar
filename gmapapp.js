0677561f0007ff6cb5b0f1a072d3d057d83ce3f5
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2021-09-03 02:19:12 +0200

    work fine v0.1

--- gmapapp.js
+++ gmapapp.js
@@ -4,14 +4,14 @@ var infowindow;
 
 
 function lunch_gmapapp(){
-    var API_Key ;//take from API_Key.txt
     
+    //IMPORTANT! Take your google api key from API_Key.txt Make sure you have turn on appropriate services
     $.get('API_Key.txt', function(data) {
-        API_Key = data;
+        var API_Key = data;
     
+        //add googleapis script to our project
         var script = document.createElement('script');
         script.src = 'https://maps.googleapis.com/maps/api/js?key=' + API_Key + '&libraries=places&callback=initMap';
-        script.async = true;
 
         // Append the 'script' element to 'head'
         document.head.appendChild(script);
@@ -19,17 +19,17 @@ function lunch_gmapapp(){
 
     // Attach your callback function to the `window` object
     window.initMap = function(place) {
-    // JS API is loaded and available
-    // var sydney = new google.maps.LatLng(-33.867, 151.195);
 
+        //TODO
         infowindow = new google.maps.InfoWindow();
         
+        //Add map object to div
         map = new google.maps.Map(
         document.getElementById('map'), {center: place, zoom: 15});
         //$('#map'),{center: place, zoom: 15});
         
         
-        //removing markers
+        //removing default markers
         const styles = {
           default: [],
           hide: [
@@ -48,67 +48,183 @@ function lunch_gmapapp(){
     };
 }
 
-
+//znajdz miejsce na podstawie nazwy wprowadzonej przez użytkownika TODO: error handling!!
 function findPlace(place){
-    var geometryPlace;
-    var service = new google.maps.places.PlacesService(map);
+    return new Promise(function(resolve, reject) {
+        var service = new google.maps.places.PlacesService(map);
 
-    var request = {
-        query: place,
-        fields: ['name', 'geometry'],
-      };
-    
-    service.findPlaceFromQuery(request, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
+        var request = {
+            query: place,
+            fields: ['name', 'geometry'],
+          };
 
-          map.setCenter(results[0].geometry.location);
-          geometryPlace = results[0].geometry.location.toJSON();
-          findNearby(geometryPlace);
-          
-        }
+         service.findPlaceFromQuery(request, function(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                //centrujemy mape na wyszukanym miejscu
+              map.setCenter(results[0].geometry.location);
+                //zwracamy dokładną nazwe miasta (poprawioną przez google api) i koordynaty geograficzne
+              let geometryPlace = {
+                  cityname: results[0].name,
+                  coord: results[0].geometry.location.toJSON()
+              }
+              
+              resolve(geometryPlace);
+            }
+        });
     });
 }
 
-function findNearby(cord){
-    const request = {
-        location: cord,
-        radius: 5000,
-        rankby: 'prominence',
-        type: ["aquarium","museum","amusement_park","art_gallery","park"] //point_of_interest
-    };
-    
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            
-                    console.log(results);
-            makeMarkers(results);
-                
-        }
+//wyszukujemy miejsca w pobliżu
+/*
+    Tutaj należy się pare słów wyjaśnienia. Google API zwraca zam 20 wyników danego zapytania.
+    W zapytaniu podajemy koordynaty geograficzne (identyfikator miejsca), 
+    typ obiektu - bez tego wyświetli nam wszystkie typy włącznie z samym miastem czy dzielnicą, bo to też są obiekty,
+    a przez to kończy nam się limit zwróconych wyników (20).
+    Google przewidziało opcje pagination dzięki, której moża by wyświetlić ponownie dane zapytanie ale z kolejnymi 20 wynikami (czyli następną stroną),
+    niemniej zaprzestałem implementacji tego rozwiązania na tym etapie. 20 wyników z danej kategorii to świat.
+*/
+function findNearby(cord,type,nextPage){
+    return new Promise(function(resolve, reject) {
+        //wyłączamy opcje Pagination
+        nextPage = false;
+        
+        const request = {
+            location: cord,
+            radius: 10000, // zasięg(promień) od podanej lokalizacji (centrum miasta), gdzie będziemy szukać miejsc TODO: stworzyć zmienną wynieść do parametru i dostosować wartość zależne od miejsca, wielkie miasto, wyspa, miasteczko powinny mieć różne promienie
+            rankby: 'prominence',
+            type: type, //point_of_interest ["aquarium","museum","amusement_park","art_gallery","park"]
+            pageToken: 'next-page-token',// pozostałość PAGITATION
+        };
+        
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, function(results, status,pagination) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+
+                //filterNearby(results);
+                if(pagination.hasNextPage && nextPage) pagination.nextPage();
+                else  resolve(results);
+            } else {
+                //bez tago występuje bug i w przypadku gdy dane miasto nie ma wszystkich zadanych typów obiektów to zacina program, jednym słowem -> TODO: ERROR HANDLING!!
+                resolve(results);
+            }
+        });
     });
 }
 
-function filterNearby(){
+//pozostałość po niedokończonej implementacji pagination
+function getAllPage(results){
     
 }
 
-function matrixNearby(){
-    const serviceGeocoder = new google.maps.Geocoder();
-    const serviceMatrix = new google.maps.DistanceMatrixService();
+function filterNearby(places){
+    //TODO: dopisać sortowanie po ratingu
+    //TODO: usunąć duplikaty, tworzy to błędy przy wytyczaniu scieżki i nie daje pełnej ilości miejsc
+    var filteredPlaces = new Array;
+    places.forEach(place => {
+        //aktualnie ustawione filtry na rating (ocena w gwiadkach na google maps) i status operational, co wyrzuca z rezultatów takie obiekty jak miasto czy dzielnica,
+        //z tym że jak teraz pomyślę to zadanie zapytania w findNearby o konkretne typy obiektów niweluje nam te typy. Przy findNearby type point_of_interest, wyświetliły by się tego typu obiekty
+        if( place.business_status = '"OPERATIONAL"' && place.rating > 4.5) {
+            filteredPlaces.push(place);          
+        }
+        
+    });
+    return filteredPlaces;
+}
+
+//tworzy tablice dystansu między danymi lokalizacjami
+function matrixNearby(places){
+    return new Promise(function(resolve, reject) {
+        //const serviceGeocoder = new google.maps.Geocoder();
+        const serviceMatrix = new google.maps.DistanceMatrixService();
+
+        //przyjmujemy tablice z dokładnymi danymi a potrzebujemy lokalizacji latlng
+        var placeLatLng = new Array;
+        places.forEach(latlang => {
+            placeLatLng.push (latlang.geometry.location);
+        });
+
+        const request = {
+            origins: placeLatLng,
+            destinations: placeLatLng,
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false,
+        };
+        //TODO: error handling
+        serviceMatrix.getDistanceMatrix(request, function(results, status){
+            if (status == google.maps.DistanceMatrixStatus.OK){
+                resolve(results)
+            }
+        });
+    });
+}
+
+//algorytm wybierający najlepszą trasę między naszymi miejscami. Aktualnie jest to jedynie iteracja i wybór najkrótszego odcinka. Ani efektywne, ani efektowne.
+function matrixBestWay(matrix){
+    
+    var iterration = matrix.rows.length;
+    var orginPath = [];
+    
+    //TODO: Zawsze zaczyna z punktu A  tj. matrix.originAddresses[0], dobrze byłoby przeiterować po każdym punkcie początkowym
+    var lowestDestDistanceDest = matrix.originAddresses[0];
+    
+    for(let i=0; i < iterration; i++){
+        orginPath.push(lowestDestDistanceDest)
+        var lowestDestDistanceValue = null;
+        var lowestDestDistanceDest = null;
+        //TODO: sprawdzić nie tylko odległość ale i czas podróży
+        for(let j = 0; j < iterration; j++){
+            if(i!=j){
+                if(lowestDestDistanceValue == null || matrix.rows[i].elements[j].distance.value < lowestDestDistanceValue ){
+                    lowestDestDistanceValue = matrix.rows[i].elements[j].distance.value;
+                    lowestDestDistanceDest = matrix.originAddresses[j];
+                }
+            }
+        }
+        
+    }
+    
+    return orginPath;
+}
+
+//tworzymy połączenia między punktami i renderujemy je na mapie
+function makeRoute(origin,destination, waypoints) {
+    //funkcja wymaga obiektu, więc korygujemy tablice
+    waypoints.forEach((wp,index) => {
+        waypoints[index] = {
+            location: wp,
+            stopover: true
+        }
+    });
+    
+    const serviceDirections = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    
+    //dodajemy wcześnie utworzoną mape do renderu
+    directionsRenderer.setMap(map);
     
     const request = {
-        origins: [, ],
-        destinations: [, ],
-        travelMode: google.maps.TravelMode.DRIVING,
-        unitSystem: google.maps.UnitSystem.METRIC,
-        avoidHighways: false,
-        avoidTolls: false,
+        origin: origin,
+        destination: destination,
+        waypoints: waypoints,
+        optimizeWaypoints: false, // google może zoptymalizować drogę ale mamy własną funkcje matrixBestWay
+        travelMode: google.maps.TravelMode.DRIVING
     };
+    //zapytanie o trase
+    serviceDirections.route(request)
+    .then((response) => {
+        //render trasy
+      directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("Directions request failed due to " + e)); //a tutaj nawet jakiś przykładowy error handling ;-)
+    
 }
 
+//dodajemy własne markery na mapie, w momencie gdy render przy makeRoute() dodał własne znaczniki te są zbędne
 function makeMarkers(places) {
     places.forEach( place => {
-        console.log(place.name + " " + place.rating);
+        
         new google.maps.Marker({
             position:place.geometry.location,
             map,
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//typy obiektów, które będziemy wyświetlać  TODO: przejrzeć i dopisać typy obiektów
+//https://developers.google.com/maps/documentation/places/web-service/supported_types
+var typesOfPlace = ["aquarium","museum","cemetery","city_hall","art_gallery","park","synagogue","church"];
+
 $(document).ready(function(){
     //dodajemy skrypt z google api TODO: error handling
     $.getScript('gmapapp.js', function(){
@@ -7,12 +11,66 @@ $(document).ready(function(){
     //wczutujemy pierwszą scene TODO: error handling
     $('#appboard').load('div/1-city-name.html', function(){
         //gdy scena wczytana dodajemy event handler
-        $('#button-search-city-name').click(function(){
+        $('#button-search-city-name').click( function(){
+            
+            //wczutujemy druga scene TODO: error handling
+            $('#appboard').load('div/2-loading.html')
+            
             //wywołujemy google api dla nazwy wpisanej w #input-city-name
-            findPlace($('#input-city-name').val());
-            $('#map').css('position', '');
-            $('#map').css('overflow', '');
+            findPlace($('#input-city-name').val()).then(function(coord){
+                //wyciągamy nazwe miasta - aby na końcu ją wyświetlić - i koordynaty dla następnego skryptu, może starczy sama nazwa miast?
+                cityname = coord.cityname;
+                coord = coord.coord;
+                
+                //tutaj trzeba zsynchronizować tyle wywołań asynchronicznych ile jest typesOfPlace
+                var fn = function findNearbytypesOfPlace(tOP){
+                    return new Promise(
+                        resolve => findNearby(coord, tOP)
+                        .then(function(places){
+                            resolve(places);
+                        })
+                    );
+                };
+                
+                //wywołujemy zapytanie o typesOfPlace
+                Promise.all(typesOfPlace.map(fn)).catch(function(err) {
+                    console.log('Error A122: A promise findPlace failed to resolve ', err);
+                }).then(values => {
+                    
+                    var allPlaces = new Array; 
+                    //scalamy odpowiedzi z zapytań w jedną tablice
+                    values.forEach(placeArr =>{
+                        placeArr.forEach(place =>{
+                            allPlaces.push(place)
+                        }); 
+                    }); 
+                    //filtrujemy miejsca patrz gmapapp.js
+                    allPlaces = filterNearby(allPlaces);
+                    
+                    //matrixApi ma ograniczenie względem ilości wyników. skracamy tablice TODO: ulepszyć skrypt, aby można było ostatecznie wyświetlić więcej atrakcji
+                    allPlaces.length = 8;
+                    matrixNearby(allPlaces).then(function(matrix){
+                        let arr = matrixBestWay(matrix);
+                        //wytyczamy trase między wybranymi atrakcjami
+                        makeRoute(arr[0],arr[arr.length-1],arr.slice(1, -1));  
+                    });
+
+                    //trzeba wyczyścić style #map aby wyświetliła się mapa google
+                    $('#map').css('position', '');
+                    $('#map').css('overflow', '');
+                    
+                    //wczutujemy trzecia scene TODO: error handling
+                    $('#appboard').load('div/3-map.html',function(){
+                        //dodajemy nazwe miasta
+                        $('.city_name').append(cityname);
+                        
+                        //dodajemy nazwy atrakcji TODO: sprawdzić i posegregować nazwy wg. kolejności
+                        allPlaces.forEach(place => {
+                            $('.attraction_list').append("<p>"+place.name+"</p>");
+                        });
+                    });
+                });
+            });
         });
     });
-    
 });
